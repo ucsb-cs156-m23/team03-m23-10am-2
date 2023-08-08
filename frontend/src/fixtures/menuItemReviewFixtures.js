fd5b93c68ceb7cd8e11f9e5d104e6232c3c2ba8f
cyrus895 <cyrusghiai-chamlou@Cyruss-MacBook-Pro.local>
2023-08-08 14:29:59 -0700

    cc- added fixtures for menuitemreview db

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,36 @@
+const menuItemReviewsFixtures = {
+    oneReview: [{
+        id: 1, 
+        itemId: 3, 
+        reviewerEmail: "cyrus895@ucsb.edu",
+        stars: 5,
+        dateReviewed: "2022-01-02T12:00:00",
+        comments: "Best food I've ever tasted. Marvelous",
+    }],
+    threeReviews: [
+        {
+            id: 1, 
+            itemId: 3, 
+            reviewerEmail: "john@gmail.com",
+            stars: 5,
+            dateReviewed: "2022-01-02T12:00:00",
+            comments: "absolutely delicious",
+        },
+        {
+            id: 2, 
+            itemId: 5, 
+            reviewerEmail: "cyrus896@ucsb.edu",
+            stars: 1,
+            dateReviewed: "2022-04-03T12:00:00",
+            comments: "it stinks. I smell a refund.",
+        },
+        {
+            id: 3, 
+            itemId: 1, 
+            reviewerEmail: "mark@yahoo.com",
+            stars: 3,
+            dateReviewed: "2022-07-04T12:00:00",
+            comments: "simply mid.",
+        }
+    ]
+};
