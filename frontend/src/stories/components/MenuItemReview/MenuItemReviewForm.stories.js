ff16aa59b9346986b6134f1d4684bba8d40323d6
cyrus895 <cyrusghiai-chamlou@Cyruss-MacBook-Pro.local>
2023-08-14 09:25:47 -0700

    fixed pre-populated fields for update form

--- frontend/src/stories/components/MenuItemReview/MenuItemReviewForm.stories.js
+++ frontend/src/stories/components/MenuItemReview/MenuItemReviewForm.stories.js
@@ -27,7 +27,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: menuItemReviewFixtures.oneReview,
+    initialContents: menuItemReviewFixtures.oneReview[0],
     buttonLabel: "Update",
     submitAction: (data) => {
         console.log("Submit was clicked with data: ", data); 
